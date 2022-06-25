10eea4d340c0fd053880a67ada232f7d9db7c261
Swiftly1 <77643169+Swiftly1@users.noreply.github.com>
2022-06-25 20:14:06 +0200

    Splitting HTML Emitting and Data Preparation (#7)

---
+++ src/Core/NodeWithDetails.cs
@@ -0,0 +1,13 @@
+﻿using CsharpToColouredHTML.Core;
+
+namespace CsharpToColouredHTML
+{
+    public record NodeWithDetails
+    (
+        Node Node,
+        string Colour,
+        bool IsNew,
+        bool IsUsing,
+        int ParenthesisCounter
+    );
+}
